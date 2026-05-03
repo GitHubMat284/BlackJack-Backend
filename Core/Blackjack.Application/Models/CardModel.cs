1e3eee8c099eb4639ef5a8ea6539f5745f3d3f21
Matheo Villacres <mathius85203@gmail.com>
2026-05-03 12:51:37 -0500

    fix: provide a card visual dto from appliation to controllers

---
+++ Core/Blackjack.Application/Models/CardModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Blackjack.Application.Models;
+
+public class CardModel
+{
+    public string Suit { get; set; } = default!;
+    public string Rank { get; set; } = default!;
+    public int Value { get; set; }
+}
--- Core/Blackjack.Application/Models/GameStateResponse.cs
+++ Core/Blackjack.Application/Models/GameStateResponse.cs
@@ -5,15 +5,22 @@ public class GameStateResponse
 {
     public required string GameID { get; set; }
     public required string Status { get; set; }
-    public IEnumerable<string> PlayerCards { get; set; } = Enumerable.Empty<string>();
-    public IEnumerable<string> DealerCards { get; set; } = Enumerable.Empty<string>();
+    public IEnumerable<CardModel> PlayerCards { get; set; } = Enumerable.Empty<CardModel>();
+    public IEnumerable<CardModel> DealerCards { get; set; } = Enumerable.Empty<CardModel>();
 
 
     public static GameStateResponse MapGameToDTO(BlackJackGame game) => new()
     {
         GameID = game.ID,
-        PlayerCards = game.Player.Hand.Cards.Select(c => c.ToString() ?? string.Empty),
-        DealerCards = game.Dealer.Hand.Cards.Select(c => c.ToString() ?? string.Empty),
-        Status = game.Status.ToString()
+        Status = game.Status.ToString(),
+        PlayerCards = game.Player.Hand.Cards.Select(ToModel).ToList(),
+        DealerCards = game.Dealer.Hand.Cards.Select(ToModel).ToList()
+    };
+
+    private static CardModel ToModel(Card c) => new()
+    {
+        Suit = c.Suit.ToString(),
+        Rank = c.Rank.ToString(),
+        Value = c.GetValue()
     };
 }
