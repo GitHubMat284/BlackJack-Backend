5b3c055888c2fc6175e8b411186d5bf6460f9cd8
Matheo Villacres <mathius85203@gmail.com>
2026-05-03 00:19:51 -0500

    feat: implement in memory game persistance

---
+++ MemoryGameDataGateway/MemoryGameDataGateway.cs
@@ -0,0 +1,29 @@
+﻿using Blackjack.Application.Interfaces;
+using BlackJack.Domain.Entities;
+using System.Collections.Concurrent;
+
+namespace MemoryGameData;
+
+/// <summary>
+/// Provides an in-memory implementation of the IGameDataGateway interface for storing and retrieving BlackJackGame
+/// instances.
+/// </summary>
+/// <remarks>This class is used with no persistent storage. All game data saved may act as volatile. This
+/// implementation is considered for testing only during early application development.</remarks>
+public class MemoryGameDataGateway : IGameDataGateway
+{
+    private readonly ConcurrentDictionary<string, BlackJackGame> _games = new();
+
+    public BlackJackGame GetByID(string id)
+    {
+        if (_games.TryGetValue(id, out var game))
+            throw new KeyNotFoundException($"Game not found with id {id}");
+       
+        return game!;
+    }
+
+    public void Save(BlackJackGame game)
+    {
+        _games[game.ID] = game;
+    }
+}
